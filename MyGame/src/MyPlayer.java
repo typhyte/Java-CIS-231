5c352fb34cdd449efcb37febe8e7129cea7ce7c2
theta <sametheta@tutanota.com>
2024-08-20 11:03:44 -0400

    end of lab

--- MyGame/src/MyPlayer.java
+++ MyGame/src/MyPlayer.java
@@ -1,5 +1,51 @@
 public class MyPlayer {
-    int id; 
-    String name;
-    int power;
+    private int id;
+    private String name;
+    private int power;
+
+    public MyPlayer(int id, String name, int power) {
+        this.id = id;
+        this.name = name;
+
+        if (power <= 2) {
+            this.power = 10;
+        } else {
+            this.power = power;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        if (power > 0) {
+            if (this.power >= power) {
+                this.power -= power;
+            } else {
+                this.power = 0;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MyPlayer \n [id=" + id + ",\n name=" + name + ",\n power=" + power + "]";
+    }
 }
