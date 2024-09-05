8e874908614a72e6257803dbb3a0c651197f3522
theta <sametheta@tutanota.com>
2024-09-05 08:14:45 -0400

    some classwork, a finished lab, and lab3 start

--- Unit-9/MyArray/src/MyArray.java
+++ Unit-9/MyArray/src/MyArray.java
@@ -11,24 +11,54 @@ public class MyArray {
 
     public static int minArray(int[] num) {
         // returns the smallest number in a user specified array
-        return 1;
+        int min = num[0];
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] < min) {
+                min = num[i];
+            }
+        }
+
+        return min;
     }
 
     public static int maxArray(int[] num) {
         //returns the largest number in a user specified array
-        return 2;
+        int max = num[0];
+        
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > max) {
+                max = num[i];
+            }
+        }
+
+        return max;
     }
 
     public static int avgArray(int[] num) {
         // calculates and returns the average of the numbers in
         // an array
-        return 1;
+        int arraySum = 0;
+        int arrayAvg = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            arraySum += num[i];
+        }
+
+        arrayAvg = arraySum / num.length;
+        
+        return arrayAvg;
     }
 
     public static int[] deepCopy(int[] num) {
         // copies a given array into a new array
-        int[] sample = {1, 2, 3};
-        return sample;
+        int[] deepCopyArray = new int[num.length];
+
+        for (int i = 0; i < num.length; i++) {
+            deepCopyArray[i] = num[i];
+        }
+
+        return deepCopyArray;
     }
 
     public static void showArray(int[] num) {
--- Unit-9/MyUtils/src/App.java
+++ Unit-9/MyUtils/src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<String> list = MyFile.readFile("DeptSheet.csv");
+
+        list.add("Lance,10,2");
+        list.add("Hank,17,1");
+
+        MyFile.writeFile(list, "DeptSheet.csv");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
