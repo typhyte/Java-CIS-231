f76b71f01010d2f4d1885edb0480cf84257c00b5
theta <sametheta@tutanota.com>
2024-09-10 08:13:09 -0400

    moved a lot of projects to current unit, plus new lab example

---
+++ Unit-11/PetShop/src/Pet.java
@@ -0,0 +1,39 @@
+public class Pet {
+    int age;
+    String species;
+    double adoptionPrice;
+    double dailyExpenses;
+    
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age > 0 && age < 10) {
+            this.age = age;
+        } else {
+            this.age = 2;
+        }
+    }
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public double getAdoptionPrice() {
+        return adoptionPrice;
+    }
+    public void setAdoptionPrice(double adoptionPrice) {
+        this.adoptionPrice = adoptionPrice;
+    }
+    public double getDailyExpenses() {
+        return dailyExpenses;
+    }
+    public void setDailyExpenses(double dailyExpenses) {
+        if (dailyExpenses > 10 && dailyExpenses < 50) {
+            this.dailyExpenses = dailyExpenses;
+        } else {
+            this.dailyExpenses = 10;
+        }
+    }
+}
