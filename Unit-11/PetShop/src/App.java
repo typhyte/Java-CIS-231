0d77c71fd49b152e30a32c9e0f1ee027154772de
theta <sametheta@tutanota.com>
2024-09-16 09:06:01 -0400

    finished assigned lab, plus .jar file lab completed

--- Unit-11/PetShop/src/App.java
+++ Unit-11/PetShop/src/App.java
@@ -2,29 +2,8 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<Pet> pet = new ArrayList();
-
-        for (int i = 0; i < 10; i++);
-            int num = (int)(Math.random() * 100 + 1) % 3;
-            switch (num) {
-                case 0:
-                    Cat cat1 = new Cat(4, 25, 12.45);
-                    pet.add(cat1);
-                    break;
-                case 1:
-                    Dog dog1 = new Dog(4, 35.50, 10.80);
-                    pet.add(dog1);
-                    break;
-                case 2:
-                    System.out.println("I am a bird!");
-                    Bird bird1 = new Bird(1, 25.50, 20.80);
-                    pet.add(bird1);
-                    break;
-            }
-
-        for (int i = 0; i < pet.size(); i++){
-            pet.get(i).speak();
-            System.out.println(pet.get(i).toString());
-        }
+        PetProcessor processPet = new PetProcessor();
+        ArrayList<Pet> petData = processPet.populatePet("PetStats.csv");
+        processPet.showList(petData);
     }
 }
